d0d5ef5bdd23f2e1cf400ca4a9f914ba90a8f662
Anas Talal <anasctalal@gmail.com>
2024-05-03 01:01:29 +0300

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -50,7 +50,7 @@
                   >
                     Last Name
                   </label>
-                    <input id="names" placeholder="Email" type="email" class=" @error('email') is-invalid @enderror flex peer text-gray-700 h-10 w-full rounded-md border border-input 
+                    <input id="names" name="email" placeholder="Email" type="email" class=" @error('email') is-invalid @enderror flex peer text-gray-700 h-10 w-full rounded-md border border-input 
                     bg-background px-3 py-2 text-sm ring-offset-background file:border-0
                     file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground
                     focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring
